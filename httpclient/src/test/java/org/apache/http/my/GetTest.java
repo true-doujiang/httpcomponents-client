bd520905737c0a56beaf8a2b213b8ccb1770357f
yhh <youhuanhuan@zhongan.com>
2021-03-07 14:27:28 +0800

    HttpGet  api测试

--- httpclient/src/test/java/org/apache/http/my/GetTest.java
+++ httpclient/src/test/java/org/apache/http/my/GetTest.java
@@ -1,22 +1,46 @@
 package org.apache.http.my;
 
 import org.apache.http.HttpEntity;
+import org.apache.http.ProtocolVersion;
+import org.apache.http.RequestLine;
+import org.apache.http.client.config.RequestConfig;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.util.EntityUtils;
+import org.apache.log4j.Logger;
 
 import java.io.IOException;
 
 public class GetTest {
 
+    private final static Logger log = Logger.getLogger(GetTest.class);
+
     public static void main(String[] args) {
-        CloseableHttpClient httpClient = HttpClients.createDefault();
+        // InternalHttpClient
+//        CloseableHttpClient httpClient = HttpClients.createDefault();
+        CloseableHttpClient httpClient = HttpClients.custom()
+                .disableAutomaticRetries()
+                .disableAuthCaching()
+                .disableRedirectHandling()
+                .disableDefaultUserAgent()
+                .build();
+
+        log.warn("httpClient: " + httpClient);
         try {
 //            HttpGet httpGet = new HttpGet("http://www.baidu.com/");
             HttpGet httpGet = new HttpGet("http://localhost:8080/test/get?userName=333");
             httpGet.setHeader("testHeader", "testValue");
+
+            // 小写http服务端就会报错
+            httpGet.setProtocolVersion(new ProtocolVersion("HTTP", 1, 1));
+
+            // 每次返回的是一个新对象
+            RequestLine requestLine = httpGet.getRequestLine();
+            ProtocolVersion protocolVersion = httpGet.getProtocolVersion();
+            RequestConfig config = httpGet.getConfig();
+
             CloseableHttpResponse response = null;
             response = httpClient.execute(httpGet);
             try {
